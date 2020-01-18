356368bc764469a33421c3dc24dc6227bbbd3ad4
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-18 18:34:27 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -19,7 +19,7 @@ end
 def total_even_pairs(src)
   even_pairs = []
   for each in src do
-    if((src[0] % 2 == 0) && (src[1] % 2 == 0 ))
+    if((src[0]%2 == 0) && (src[1] % 2 == 0 ))
       even_pairs.push(each)
     end
   end
