94919a88de4ce4a21c5a8be30d10c3cd5d3cfd59
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-18 18:44:58 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -20,7 +20,9 @@ def total_even_pairs(src)
   temp_sum = 0
   puts src[0]
   for each in src do
-    if
+    if(each[0].even && each[1].even)
+      temp_sum += (each[0] + each[1])
+    end
   end
   
   return temp_sum
